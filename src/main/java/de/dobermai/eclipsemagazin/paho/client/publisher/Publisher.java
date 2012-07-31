f4530505e48a5cf90340fbde6a61c5d3818debd2
Dominik Obermaier <dominik.obermaier@dc-square.de>
2012-07-31 14:01:54 +0200

    added publisher

---
+++ src/main/java/de/dobermai/eclipsemagazin/paho/client/publisher/Publisher.java
@@ -0,0 +1,74 @@
+package de.dobermai.eclipsemagazin.paho.client.publisher;
+
+import de.dobermai.eclipsemagazin.paho.client.util.Utils;
+import org.eclipse.paho.client.mqttv3.*;
+
+/**
+ * @author Dominik Obermaier
+ */
+public class Publisher {
+
+    public static final String BROKER_URL = "tcp://broker.mqttdashboard.com:1883";
+    //public static final String BROKER_URL = "tcp://test.mosquitto.org:1883";
+
+    public static final String TOPIC_BRIGHTNESS = "eclipsemagazin/homeautomation/brightness";
+    public static final String TOPIC_TEMPERATURE = "eclipsemagazin/homeautomation/temperature";
+
+    private MqttClient client;
+
+
+    public Publisher() {
+
+        //We have to generate a unique Client id.
+        String clientId = Utils.getMacAddress() + "-pub";
+
+
+        try {
+            client = new MqttClient(BROKER_URL, clientId);
+
+
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(false);
+
+            client.connect(options);
+
+            //Publish data forever
+            while (true) {
+
+                publishBrightness();
+
+                Thread.sleep(1000);
+
+                publishTemperature();
+
+                Thread.sleep(1000);
+            }
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void publishTemperature() throws MqttException {
+        final MqttTopic temperatureTopic = client.getTopic(TOPIC_TEMPERATURE);
+        final int temperatureNumber = Utils.createRandomNumberBetween(20, 30);
+        final String temperature = temperatureNumber + " °C";
+        temperatureTopic.publish(new MqttMessage(temperature.getBytes()));
+        System.out.println("Published data. Topic: " + temperatureTopic.getName() + " Message: " + temperature);
+    }
+
+    private void publishBrightness() throws MqttException {
+        final MqttTopic brightnessTopic = client.getTopic(TOPIC_BRIGHTNESS);
+
+        final int brightnessNumber = Utils.createRandomNumberBetween(0, 100);
+        final String brigthness = brightnessNumber + "%";
+        brightnessTopic.publish(new MqttMessage(brigthness.getBytes()));
+        System.out.println("Published data. Topic: " + brightnessTopic.getName() + "   Message: " + brigthness);
+    }
+
+    public static void main(String... args) {
+        final Publisher publisher = new Publisher();
+    }
+}
